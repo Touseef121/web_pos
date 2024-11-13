72f8f9bf4d309e95417e40c94870f59e848b3079
Touseef Akram <111420672+Touseef121@users.noreply.github.com>
2024-11-13 14:30:42 +0500

    Edit

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command(ResetEmployeeSalaryStatus::class)->everyHour();
+Schedule::command(ResetEmployeeSalaryStatus::class)->daily();
